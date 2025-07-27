0e6b76634ff9050fe39b182d2e917de1a0aff94c
neharajbanshi896@gmail.com <neharajbanshi@Nehas-Air.bbrouter>
2025-07-27 10:35:04 +0545

    Register models for ChatbotSession, Subject, Lesson, Quiz, UserProgress, and UserProfile in the admin interface

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ChatbotSession
 
 # Register your models here.
+admin.site.register(ChatbotSession)
--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Subject, Lesson
 
 # Register your models here.
+admin.site.register(Subject)
+admin.site.register(Lesson)
